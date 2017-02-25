d9aab8cb53270279a89c93ceef8eed692328672e
Yurii Prokulevych <yprokule@redhat.com>
2017-02-25 18:12:53 +0100

    String interpolation in Ruby

---
+++ string_interpolation.rb
@@ -0,0 +1,11 @@
+#!/bin/env ruby
+
+var1 = 3
+var2 = 5
+
+puts "#{var1} * #{var2} = #{ var1 * var2}"
+puts "#{var1} + #{var2} = #{ var1 + var2}"
+puts "#{var1} - #{var2} = #{ var1 - var2}"
+puts "#{var1} / #{var2} = #{ var1 / var2}"
+puts "#{var1} > #{var2} = #{ var1 > var2}"
+puts "#{var1} < #{var2} = #{ var1 < var2}"
