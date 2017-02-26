0d8b60f5f458bac6bbf217623f53c4ac43ca0a33
Yurii Prokulevych <yprokule@redhat.com>
2017-02-26 15:58:34 +0100

    Setting class variables

--- class_person.rb
+++ class_person.rb
@@ -5,16 +5,27 @@
 
 class Person
 
+    def set_name=(p_name)
+        @name = p_name
+    end
+
+    def set_age=(p_age)
+        @age = p_age
+    end
+
     def introduce
-        puts "My name is: "
+        puts "My name is: #{@name}"
     end
 
     def age
-        puts "My age is: "
+        puts "My age is: #{@age}"
     end
 end
 
 person1 = Person.new
+
+person1.set_name = 'Tom'
+person1.set_age = 15
 
 person1.introduce
 person1.age
