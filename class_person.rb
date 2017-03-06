8c1d02cdb45261ce92fd35b022febe70bd229735
Yurii Prokulevych <yprokule@redhat.com>
2017-03-06 19:56:58 +0100

    Removed commented code. Overwritten to_s method

--- class_person.rb
+++ class_person.rb
@@ -5,27 +5,39 @@
 
 class Person
 
-    def set_name=(p_name)
-        @name = p_name
-    end
-
-    def set_age=(p_age)
-        @age = p_age
-    end
+    attr_accessor :name, :age
 
     def introduce
         puts "My name is: #{@name}"
     end
 
-    def age
+    def print_age
         puts "My age is: #{@age}"
+    end
+
+    def raise_message
+        raise "Raise is called"
+    end
+
+    def to_s
+        # over-write method
+        "PERSON. Name #{@name}\nPERSON. Age: #{@age}"
     end
 end
 
 person1 = Person.new
 
-person1.set_name = 'Tom'
-person1.set_age = 15
+#person1.set_name = 'Tom'
+#person1.set_age = 15
+
+person1.age=18
+person1.name='Tommy'
 
 person1.introduce
-person1.age
+person1.print_age
+
+puts '----------------------------------------'
+puts person1.name
+puts person1.age
+puts person1
+puts '----------------------------------------'
